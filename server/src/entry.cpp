26600363ff4a5ab020de52b5167942c9054ee5d4
Todd <toddmoulder88@gmail.com>
2016-02-02 12:21:01 -0600

    Entry Point for Server and Card Database

---
+++ server/src/entry.cpp
@@ -0,0 +1,38 @@
+   // Server Entry Point
+   // LSU Letter
+   // CSC3380 Class Project
+   // Authors:  Hardika Patel
+   //           Jonathan Nguyen
+   //           Todd Moulder
+   //           William Raziano
+
+   #include <iostream>
+   #include <sockets>
+   #include <threads>
+   #include <vector>
+
+   using namespace std;
+
+   const char * cardLocationStr = "../txt/cards.data";
+
+   bool startServer( int port );
+   void serverLoop();
+   void shutDownServer();
+
+   int main( int argc, char * argv[] ){
+     // Server Initialization
+   	 // Server Main Loop
+   	 // Server ShutDown
+   	return 0;
+   }
+
+   bool startServer( int port ){
+   	bool success = false;
+   	// Load Cards
+   	// Start Lobby
+   	// Start Game0
+   	// set up listen(), maybe? listen may have to be in threadpool
+     return success;
+   }
+   void serverLoop();
+   void shutDownServer();
