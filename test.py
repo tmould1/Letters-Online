a5b2436968faec0166298f75b948d6dcdd4a3392
DESKTOP-2P3T0MG\tmoul <toddmoulder88@gmail.com>
2016-10-29 16:00:43 -0500

    asdasd

---
+++ test.py
@@ -0,0 +1,36 @@
+import socket
+import select
+
+testSock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+
+def Connect( mySock ):
+    mySock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    mySock.connect( ('toddmoulder.com', 9999) )
+
+def Disconnect( mySock ):
+    mySock.send("Disconnect")
+
+def Login(mySock):
+    mySock.send("Login Todd test 127.0.0.1");
+    var = mySock.recv( 1024 )
+    print var
+    
+def TestRun():
+    global testSock
+    testSock.connect( ('toddmoulder.com', 9999) )
+    testSock.send("NewAccount Ryno onyr 255.255.255.255 ryno@onyr.com false")
+    serverMsg = testSock.recv( 1024 )
+    print serverMsg
+    testSock.send("CreateGame TestGame 4 Todd")
+    serverMsg = testSock.recv( 1024 )
+    print serverMsg   
+
+    
+
+def Disconnect():
+    global testSock
+    testSock.send("Disconnect")
+
+def CreateGame( name, maxPlayers, creatorName ) :
+    testSock.send("CreateGame" + name + str(maxPlayers) + creatorName)
+
